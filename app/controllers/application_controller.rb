d50a1c1d03d1dc0948c9111fe4bb3a3051f99ce3
EC2 Default User <ibanez19851030@yahoo.co.jp>
2019-02-02 19:24:15 +0000

    scroll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     @current_user = User.find_by(id: session[:user_id])
   end
   
+  
   def authenticate_user
     if @current_user == nil
       flash[:info] = "ログインが必要です"
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
  ## before_action :forbid_login_user, {only: [:top]}
   
   def top
+   
   end
   
   def about
